7b9b9bd1f74997eefdcb35a8c2c21fb7c9695443
Virgilio T. Dato <virgiliodato@gmail.com>
2017-08-15 05:55:47 +0800

    Passing data to child components

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'events-list',
-  templateUrl: 'app/events/events-list.component.html'
+  template: `
+    <div>
+        <h2>Upcoming Angular 2 Events</h2>
+        <event-thumbnail [event]="event1"></event-thumbnail>
+    </div>
+  `
 })
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2018',
