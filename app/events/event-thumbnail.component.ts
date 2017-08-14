aefe9b89ae3619f03a81d0420c7f555f2b5214c0
Virgilio T. Dato <virgiliodato@gmail.com>
2017-08-15 07:51:45 +0800

    Applying styles

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,20 +1,24 @@
 import { Component, Input, Output,  EventEmitter } from '@angular/core'
 
 @Component({
-  selector: 'event-thumbnail',
-  template: `
-    <div class="well hoverwell thumbnail">
-    <h3>{{event.name}}</h3>
-    <div>Date: {{event.date}}</div>
-    <div>Time: {{event.time}}</div>
-    <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+    selector: 'event-thumbnail',
+    template: `
+        <div class="well hoverwell thumbnail">
+        <h3>{{event.name}}</h3>
+        <div>Date: {{event.date}}</div>
+        <div>Time: {{event.time}}</div>
+        <div>Price: \${{event.price}}</div>
+            <div>
+                <span>Location: {{event.location.address}}</span>
+                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            </div>
         </div>
-    </div>
-    `
+        `,
+    styles:[`
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb }
+    `]
+
 })
 export class EventThumbnail {
     @Input() event: any
