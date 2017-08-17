4e5de94d0b9d0039353ba0c3133f7b9647bf5103
Virgilio T. Dato <virgiliodato@gmail.com>
2017-08-17 13:28:37 +0800

    Show details on click

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,16 +3,16 @@ import { Component, Input, Output,  EventEmitter } from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template: `
-        <div class="well hoverwell thumbnail">
-        <h3>{{event?.name}}</h3>
-        <div>Date: {{event?.date}}</div>
-        <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time"> 
-            Time: {{event?.time}}
-            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
-            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-            <span *ngSwitchDefault>(Normal Start)</span>
-        </div>
-        <div>Price: \${{event?.price}}</div>
+        <div [routerLink]="['/events',event.id]" class="well hoverwell thumbnail">
+            <h3>{{event?.name}}</h3>
+            <div>Date: {{event?.date}}</div>
+            <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time"> 
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
+            <div>Price: \${{event?.price}}</div>
             <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
                 <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
