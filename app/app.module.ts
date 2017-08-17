4935b7f274e50a8c7ac0e4de3f1cbd7abc6eac4a
Virgilio T. Dato <virgiliodato@gmail.com>
2017-08-17 13:59:12 +0800

    Create and Cancel Routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { CreateEventComponent } from './events/create-event.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
@@ -20,6 +21,7 @@ import { appRoutes } from './route'
     EventsAppComponent, 
     EventsListComponent, 
     EventThumbnailComponent,
+    CreateEventComponent,
     EventDetailsComponent,
     NavBarComponent],
   providers: [EventService, ToastrService],
